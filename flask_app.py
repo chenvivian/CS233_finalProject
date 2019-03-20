2fd0aeabeefb0f7c47223615d42107411c4f5e66
cs232av <vivianc206@gmail.com>
2019-03-20 22:41:58 +0000

    completed draft for cart and comment input

--- flask_app.py
+++ flask_app.py
@@ -2,18 +2,39 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-comments = []
-cids = []
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="cs232av",
+    password="Computer1",
+    hostname="cs232av.mysql.pythonanywhere-services.com",
+    databasename="cs232av$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+    cid = db.Column(db.Integer, nullable=False)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=comments, cids=cids)
+        return render_template("main_page.html", comments=Comment.query.all())
 
-    cids.append(request.form["cid"])
-    comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"], cid=request.form["cid"])
+
+    db.session.add(comment)
+    db.session.commit()
+
     return redirect(url_for('index'))
