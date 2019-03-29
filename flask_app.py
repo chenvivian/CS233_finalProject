d42561c3566c54510efaa94b10b5de9b2facefc5
cs232av <vivianc206@gmail.com>
2019-03-29 20:14:09 +0000

    added comments for each cart functionality

--- flask_app.py
+++ flask_app.py
@@ -113,23 +113,18 @@ def signup():
 
     return redirect(url_for('login'))
 
-@app.route("/carts/")
+@app.route("/carts")
 def carts():
     return render_template("carts_page.html", carts=Carts.query.all())
 
 @app.route("/carts/<cid>", methods=["GET","POST"])
-
 def cartComments(cid):
     if request.method == "GET":
         comments = Comment.query.filter_by(cid=cid)
         cartName = Carts.query.filter_by(id=cid)
         return render_template("carts_page.html", carts=Carts.query.all(), comments=comments, cartName=cartName)
 
-    if not current_user.is_authenticated:
-        return redirect(url_for('carts_page'))
-
-    comment = Comment(content=request.form["contents"], cid=cid, commenter=current_user)
-    db.session.add(comment)
-    db.session.commit()
-
-    return redirect()
+    if request.method == "POST":
+        comment = Comment(content=request.form["contents"], cid=cid, commenter=current_user)
+        db.session.add(comment)
+        db.session.commit()
